999cde272e36447d60609ab8cc02995b648c6f55
speleo27 <seb10400@orange.fr>
2022-02-17 11:59:02 +0100

    feat(wip) homepage

--- wp-content/themes/veilleTech/index.php
+++ wp-content/themes/veilleTech/index.php
@@ -1,6 +1,30 @@
 <?php get_header(); ?>
+	<header>
+		<div class="container">
+			<div class="flex flex-col">
+				<h1><a href="<?php echo home_url( '/' ); ?>">La veille techno</a></h1>
+			</div>
+			<div class="col-4">
+			</div>
+			<div class="col-2">
+				<svg xmlns="http://www.w3.org/2000/svg" width="42.938" height="39.733" viewBox="0 0 42.938 39.733">
+					<g id="Composant_8_1" data-name="Composant 8 – 1" transform="translate(0.5 0.5)">
+						<path id="Tracé_2" data-name="Tracé 2" d="M3,4H44.938V8.842H3ZM3,20.946H44.938v4.842H3ZM3,37.892H44.938v4.842H3Z" transform="translate(-3 -4)" fill="#fff" stroke="#3204d9" stroke-width="1"/>
+					</g>
+				</svg>
+			</div>
+		</div>
+	</header>
 
-	<h1>Coucou</h1>
-	<h2>Ceci est mon premier thème personnalisé en wordpress</h2>
+	<div class="container">
+		<div class="homeTitle">
+			<h2>Acceuil</h2>
+			<p>Ce blog a pour but d'informer sur toutes les nouveautés dans le monde du
+				numérique. Les informations sont classées par catégorie. Si vous souhaitez
+				contribuer vous pouvez faire la demande par mail
+				<a href="mailto:webmaster@laboiteadev.fr">faire la demande par mail</a></p>
+		</div>
+
+	</div>
 
 <?php get_footer(); ?>
