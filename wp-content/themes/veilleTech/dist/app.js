8f429c865129292cc271560f465c9bc6a01e435b
speleo27 <seb10400@orange.fr>
2022-03-17 16:14:11 +0100

    Feat(color) add color config not operante

---
+++ wp-content/themes/veilleTech/dist/app.js
@@ -0,0 +1,2 @@
+const t={Elem:document.getElementById("hamburger"),status:!1},e=document.getElementById("menu");t.Elem.addEventListener("click",(()=>{!0===t.status?t.Elem.style.transform="initial":t.Elem.style.transform="rotate(90deg)",t.status=!t.status,e.classList.toggle("d-none")}));
+//# sourceMappingURL=index.c64332fb.js.map
